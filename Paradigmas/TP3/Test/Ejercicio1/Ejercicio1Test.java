ab7c979d970ae6853c5b3ac917ad982a97abd298
Gastón Suárez <suarez.gastonalejandro@gmail.com>
2016-09-06 08:16:15 -0300

    TP3 Ejercicio1
    Creación paquete Ejercicio3 y Realización Ejercicio1 y su
    correspondiente Test

---
+++ Paradigmas/TP3/Test/Ejercicio1/Ejercicio1Test.java
@@ -0,0 +1,37 @@
+package Ejercicio1;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by GastónAlejandro on 06/09/2016.
+ */
+public class Ejercicio1Test {
+    private Ejercicio1 ejercicio1;
+
+    @Before
+
+    public void setUp() throws Exception {
+        ejercicio1 = new Ejercicio1();
+    }
+
+    @Test
+    public void retornaResultado() throws Exception {
+        Assert.assertTrue(180 == ejercicio1.RetornaResultado("15 * 12"));
+    }
+    @Test
+    public void retornaSuma() throws Exception {
+        Assert.assertTrue(27 == ejercicio1.RetornaResultado("15 + 12"));
+    }
+    @Test
+    public void retornaResta() throws Exception {
+        Assert.assertTrue(3 == ejercicio1.RetornaResultado("15 - 12"));
+    }
+    @Test
+    public void retornaDiv() throws Exception {
+        Assert.assertTrue(1 == ejercicio1.RetornaResultado("15 / 12"));
+    }
+}
