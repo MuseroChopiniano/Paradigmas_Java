fdee3310182985c42f8bdbaa376138fa06a3fba8
FacundoTripelhorn <facundo.tripelhorn@gmail.com>
2016-09-18 22:14:59 -0300

    TP3 Ejercicio 4 Expresiones balanceadas

---
+++ Paradigmas/TP3/Test/ejercicio4/Ejercicio4Test.java
@@ -0,0 +1,26 @@
+package ejercicio4;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Facu on 18/9/2016.
+ */
+public class Ejercicio4Test {
+
+    private Ejercicio4 ejercicio4;
+    @Before
+    public void setUp() throws Exception {
+this.ejercicio4 = new Ejercicio4();
+    }
+
+    @Test
+    public void balanceo() throws Exception {
+        boolean resultado=false;
+        Assert.assertSame(resultado, ejercicio4.Balanceo("{123+2+[x+3[y*32](z+x)]}"));
+    }
+
+}
---
+++ Paradigmas/TP3/src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,54 @@
+package ejercicio4;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+/**
+ * Created by Facu on 18/9/2016.
+ */
+public class Ejercicio4 {
+
+    boolean resultado=true;
+
+    public boolean Balanceo(String expresion) {
+        Stack<String> pila = new Stack<String>();
+        for (int i = 0; i < expresion.length(); i++){
+            switch (expresion.charAt(i)) {
+                case '{':
+                    if (pila.isEmpty()){
+                    pila.push("{");
+                    }
+                    break;
+                case '[':
+                    if(pila.isEmpty()&&pila.lastElement()=="{"){
+                        pila.push("[");
+                    }
+                    break;
+                case '(':
+                    pila.push("(");
+                    break;
+            }
+
+            switch (expresion.charAt(i)) {
+                case '}':
+                    if (pila.lastElement() != "{") {
+                        resultado = false;
+                    } else pila.pop();
+                    break;
+                case ']':
+                    if (pila.lastElement() != "[") {
+                        resultado = false;
+                    }else pila.pop();
+                    break;
+                case ')':
+                    if (pila.lastElement() != "(") {
+                        resultado = false;
+                    }else pila.pop();
+                    break;
+            }
+        }
+        if (pila.isEmpty()){
+            return resultado;
+        } else return false;
+    }
+}
