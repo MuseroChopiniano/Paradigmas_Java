c77dc0cde30d45ceb28016614cce21fe6bed61de
Gastón Suárez <suarez.gastonalejandro@gmail.com>
2016-09-19 02:18:11 -0300

    TP4 inicio

---
+++ Paradigmas/TP4/src/bdrestaurant/BaseDatos.java
@@ -0,0 +1,60 @@
+package bdrestaurant;
+
+import java.sql.*;
+/**
+ * Created by GastónAlejandro on 19/09/2016.
+ */
+public class BaseDatos
+{
+    private static String ConnStr="JDBC:h2:C:/restaurant";
+    public static ResultSet Consulta (String Comando)
+    {
+        ResultSet retorno=null;
+        try {
+            Class.forName("org.h2.Driver");
+                Connection conexion = DriverManager.getConnection(ConnStr,"sa","");
+                Statement sentencia = conexion.createStatement();
+            retorno=  sentencia.executeQuery(Comando);
+            sentencia.close();
+            conexion.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return  retorno;
+    }
+
+    public static void Actualizar (String comando)
+    {
+        try
+        {
+            Class.forName("org.h2.Driver");
+            Connection conexion = DriverManager.getConnection(ConnStr,"sa","");
+            Statement sentencia = conexion.createStatement();
+            sentencia.executeUpdate(comando);
+            sentencia.close();
+            conexion.close();
+        }
+        catch (ClassNotFoundException ex){}
+        catch (SQLException ex){}
+
+    }
+    public static void BorrarTodo(String Tabla)
+    {
+        try {
+            Class.forName("org.h2.Driver");
+            Connection conexion = DriverManager.getConnection(ConnStr,"sa","");
+            Statement sentencia = conexion.createStatement();
+            sentencia.executeUpdate("DELETE * FROM " + Tabla);
+            sentencia.close();
+            conexion.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ Paradigmas/TP4/src/bdrestaurant/IngredienteDAO.java
@@ -0,0 +1,32 @@
+package bdrestaurant;
+
+import java.util.List;
+
+/**
+ * Created by GastónAlejandro on 19/09/2016.
+ */
+public class IngredienteDAO
+{
+    public static void altaIngrediente(String nombre, String tipo)
+    {
+
+    }
+    public static void borrarIngrediente(String nombre)
+    {
+
+    }
+    public static void modificarIngrediente(String nombreViejo, String nombreNuevo,String tipoNuevo)
+    {
+
+    }
+    public static List<String> devolverIngredientes()
+    {
+        List<String> Lista = null;
+        return  Lista;
+    }
+    public static List<String> devolverLasVerduras()
+    {
+        List<String> Lista= null;
+        return  Lista;
+    }
+}
